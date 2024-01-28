d9e20bc387a313c680f50034d4c215981dc6de74
alfnsnff <alfnsnff@gmail.com>
2024-01-28 11:59:37 +0700

    Add google provider

--- src/app/auth/signup/page.tsx
+++ src/app/auth/signup/page.tsx
@@ -7,14 +7,17 @@ import {
   createUserWithEmailAndPassword,
   signInWithPopup,
   GoogleAuthProvider,
+  updateProfile,
 } from "firebase/auth";
 import { useRouter } from "next/navigation";
 import Link from "next/link";
+import { Button } from "@/components/ui/button";
 
 export default function SignUp() {
   const router = useRouter();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [username, setUsername] = useState("");
 
   const signInWithGoogle = async () => {
     signInWithPopup(auth, googleProvider)
@@ -44,9 +47,14 @@ export default function SignUp() {
   const onSubmit = async (e: any) => {
     e.preventDefault();
     createUserWithEmailAndPassword(auth, email, password)
-      .then((userCredential) => {
+      .then(async (userCredential) => {
         // Signed up
         const user = userCredential.user;
+        if (user) {
+          await updateProfile(user, {
+            displayName: username,
+          });
+        }
         router.push("/auth/signin");
         console.log("Sign Up successfully");
         // ...
@@ -59,51 +67,86 @@ export default function SignUp() {
   };
 
   return (
-    <div className="flex justify-center m-24">
-      <form className="size-1/2">
-        <div>
-          <p className="text-4xl font-bold w-full my-4">Register</p>
-        </div>
-        <div className="mb-[22px]">
-          <input
-            type="email"
-            placeholder="Email"
-            name="email"
-            id="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-            className="w-full rounded-md border border-stroke bg-transparent px-5 py-3 text-base text-dark outline-none transition placeholder:text-dark-6 focus:border-primary focus-visible:shadow-none dark:border-dark-3 dark:text-white dark:focus:border-primary"
-          />
-        </div>
-        <div className="mb-[22px]">
-          <input
-            type="password"
-            placeholder="Password"
-            name="password"
-            required
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            className="w-full rounded-md border border-stroke bg-transparent px-5 py-3 text-base text-dark outline-none transition placeholder:text-dark-6 focus:border-primary focus-visible:shadow-none dark:border-dark-3 dark:text-white dark:focus:border-primary"
-          />
-        </div>
-        <div className="mb-9 space-y-2">
-          <button
-            type="submit"
-            onClick={onSubmit}
-            className="w-full cursor-pointer rounded-md border border-primary bg-primary px-5 py-3 text-base text-white transition duration-300 ease-in-out hover:bg-blue-dark"
-          >
-            Sign Up
-          </button>
-          <button
-            type="submit"
+    <div className="bg-[#F4F7FF] h-screen py-12">
+      <div className="container mx-auto">
+        <div className="mx-auto max-w-[525px] rounded-xl bg-white px-12 py-16 shadow-sm">
+          <p className="text-4xl font-bold w-full mb-8">Sign up</p>
+          <form className="">
+            <div className="mb-[22px]">
+              <input
+                type="email"
+                placeholder="Email"
+                name="email"
+                id="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+                className="w-full rounded-md border border-stroke bg-transparent px-5 py-3 text-base text-dark outline-none transition placeholder:text-dark-6 focus:border-primary focus-visible:shadow-none dark:border-dark-3 dark:text-white dark:focus:border-primary"
+              />
+            </div>
+            <div className="mb-[22px]">
+              <input
+                type="text"
+                placeholder="Username"
+                name="username"
+                id="username"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                required
+                className="w-full rounded-md border border-stroke bg-transparent px-5 py-3 text-base text-dark outline-none transition placeholder:text-dark-6 focus:border-primary focus-visible:shadow-none dark:border-dark-3 dark:text-white dark:focus:border-primary"
+              />
+            </div>
+            <div className="mb-[22px]">
+              <input
+                type="password"
+                placeholder="Password"
+                name="password"
+                required
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                className="w-full rounded-md border border-stroke bg-transparent px-5 py-3 text-base text-dark outline-none transition placeholder:text-dark-6 focus:border-primary focus-visible:shadow-none dark:border-dark-3 dark:text-white dark:focus:border-primary"
+              />
+            </div>
+            <div className="space-y-2">
+              <Button
+                type="submit"
+                onClick={onSubmit}
+                className="h-11 w-full cursor-pointer rounded-md border border-primary bg-primary text-base text-white transition duration-300 ease-in-out hover:bg-slate-500"
+              >
+                Sign Up
+              </Button>
+            </div>
+          </form>
+          <span className="relative my-4 block text-center text-slate-800">Connect With</span>
+          <Button
             onClick={signInWithGoogle}
-            className="w-full cursor-pointer rounded-md px-5 py-3 text-base text-white transition duration-300 ease-in-out bg-red-500 hover:bg-blue-dark"
+            className="flex h-11 w-full mb-8 gap-2 items-center justify-center rounded-md bg-[#D64937] text-white transition hover:bg-[#ff5858]"
           >
+            <svg
+              width="18"
+              height="18"
+              viewBox="0 0 18 18"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M17.8477 8.17132H9.29628V10.643H15.4342C15.1065 14.0743 12.2461 15.5574 9.47506 15.5574C5.95916 15.5574 2.8306 12.8821 2.8306 9.01461C2.8306 5.29251 5.81018 2.47185 9.47506 2.47185C12.2759 2.47185 13.9742 4.24567 13.9742 4.24567L15.7024 2.47185C15.7024 2.47185 13.3783 0.000145544 9.35587 0.000145544C4.05223 -0.0289334 0 4.30383 0 8.98553C0 13.5218 3.81386 18 9.44526 18C14.4212 18 17.9967 14.7141 17.9967 9.79974C18.0264 8.78198 17.8477 8.17132 17.8477 8.17132Z"
+                fill="white"
+              />
+            </svg>
             Google
-          </button>
+          </Button>
+          <p className="text-body-secondary text-base">
+            Not a member yet?{" "}
+            <Link
+              href="/auth/signup"
+              className="text-slate-800 hover:underline"
+            >
+              Sign Up
+            </Link>
+          </p>
         </div>
-      </form>
+      </div>
     </div>
   );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -33,7 +33,7 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { useRouter } from "next/navigation";
-import { getAuth, onAuthStateChanged, signOut } from "firebase/auth";
+import { onAuthStateChanged, signOut } from "firebase/auth";
 import { auth } from "@/lib/firebase-config";
 import Link from "next/link";
 import { useState, useEffect } from "react";
@@ -102,17 +102,17 @@ export default function Navbar() {
           {user ? (
             <DropdownMenu>
               <DropdownMenuTrigger asChild>
-                <button
+                <Button
                   type="button"
-                  className="relative infline-flex items-center justify-start inline-block px-4 py-2 overflow-hidden font-bold rounded-full group"
+                  className="relative infline-flex items-center justify-start inline-block px-4 py-2 overflow-hidden font-bold rounded-full group bg-white"
                 >
                   <span className="w-32 h-32 rotate-45 translate-x-12 -translate-y-2 absolute left-0 top-0 bg-black opacity-[3%]"></span>
                   <span className="absolute top-0 left-0 w-48 h-48 -mt-1 transition-all duration-500 ease-in-out rotate-45 -translate-x-56 -translate-y-24 bg-black opacity-100 group-hover:-translate-x-8"></span>
                   <span className="relative w-full text-left text-black transition-colors duration-200 ease-in-out group-hover:text-white">
-                    {user.email}
+                    {user.displayName}
                   </span>
                   <span className="absolute inset-0 border-2 border-black rounded-full"></span>
-                </button>
+                </Button>
               </DropdownMenuTrigger>
               <DropdownMenuContent className="w-56">
                 <DropdownMenuLabel>My Account</DropdownMenuLabel>
@@ -156,7 +156,12 @@ export default function Navbar() {
                 </span>
                 <span className="absolute inset-0 border-2 border-black rounded-full"></span>
               </Link>
-              <button onClick={() => console.log(user)}>Click me</button>
+              <Link
+                href={"/auth/signup"}
+                className="text-black transition-colors duration-200 ease-in-out hover:text-slate-500"
+              >
+                Sign up
+              </Link>
             </>
           )}
         </div>
