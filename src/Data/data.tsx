1643d23d409090b1e8f8518f53c3b5ab660bcc5b
Noitusanx <habilnasutionx@gmail.com>
2024-01-30 18:16:39 +0700

    fix: icon at data.tsx

--- src/Data/data.tsx
+++ src/Data/data.tsx
@@ -1,17 +1,16 @@
-import { iconsImgs } from "@/Utils/Image";
-// import { personsImgs } from "@/Utils/Image";
+import {home, budget, plane, wallet, bills, report, wealth, user, gears} from '../../public/assets/icons'
 
 export const navigationLinks = [
-    { id: 1, title: "Home", image: iconsImgs.home, link: "/Home" },
-    { id: 2, title: "Budget", image: iconsImgs.budget },
-    { id: 3, title: "Transactions", image: iconsImgs.plane },
-    { id: 4, title: "Subscriptions", image: iconsImgs.wallet },
-    { id: 5, title: "Loans", image: iconsImgs.bills },
-    { id: 6, title: "Reports", image: iconsImgs.report },
-    { id: 7, title: "Savings", image: iconsImgs.wallet },
-    { id: 8, title: "Financial Advice", image: iconsImgs.wealth },
-    { id: 9, title: "Account", image: iconsImgs.user },
-    { id: 10, title: "Settings", image: iconsImgs.gears },
+    { id: 1, title: "Home", image: home, link: "/Home" },
+    { id: 2, title: "Budget", image: budget },
+    { id: 3, title: "Transactions", image: plane },
+    { id: 4, title: "Subscriptions", image: wallet },
+    { id: 5, title: "Loans", image: bills },
+    { id: 6, title: "Reports", image: report },
+    { id: 7, title: "Savings", image: wallet },
+    { id: 8, title: "Financial Advice", image: wealth },
+    { id: 9, title: "Account", image: user },
+    { id: 10, title: "Settings", image: gears },
 ];
 
 export const problemCard = [
