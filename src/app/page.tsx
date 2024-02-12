36aba249bdae203856d947f16acd1a5aa363209f
alfnsnff <alfnsnff@gmail.com>
2024-02-12 15:40:57 +0700

    Fix after merge

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,15 @@
-import ContentLayout from '@/Layouts/ContentLayout';
-import Hero from '@/components/Home/Hero';
-import Step from '@/components/Home/Step';
-import Rank from '@/components/Home/Rank';
-import Cta from '@/components/Home/Cta';
+import Hero from "@/components/Home/Hero";
+import Step from "@/components/Home/Step";
+import Rank from "@/components/Home/Rank";
+import Cta from "@/components/Home/Cta";
 
-// import { Metadata } from 'next';
-
-// ! Masih harus di fix bagian authnya
-
-interface HomeProps {
-  auth?: { user: any };
-}
-
-// export const metadata: Metadata = {
-//     title: 'TCC',
-//   };
-
-const Home: React.FC<HomeProps> = ({ auth }) => {
+export default function Home() {
   return (
-    <ContentLayout user={auth?.user}>
+    <div>
       <Hero />
       <Step />
       <Rank />
       <Cta />
-    </ContentLayout>
+    </div>
   );
 };
-
-export default Home;
-
