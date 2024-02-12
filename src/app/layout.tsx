2403ff4564b3c179db91552addeacf91c0954ca8
alfnsnff <alfnsnff@gmail.com>
2024-02-12 20:37:43 +0700

    Rollback Mixed-Content HTTP error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,12 +14,6 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head>
-        <meta
-          http-equiv="Content-Security-Policy"
-          content="upgrade-insecure-requests"
-        />
-      </head>
       <body className={inter.className}>
         <AuthContextProvider>
           <Header />
--- src/components/Problems/Loading/index.tsx
+++ src/components/Problems/Loading/index.tsx
@@ -1,21 +1,10 @@
 import { Skeleton } from "../../ui/skeleton";
-import { Card, CardContent } from "../Card";
+import { Card } from "../Card";
 
 export default function ProblemsLoader() {
   return (
     <div className="container mx-auto my-12">
-      <div className="flex gap-4">
-        <Card className="min-w-80 h-max">
-          <CardContent>
-            <Skeleton className="h-4 w-[100px]" />
-            <div className="flex flex-col space-y-2">
-              <Skeleton className="h-4 w-full" />
-              <Skeleton className="h-4 w-full" />
-              <Skeleton className="h-4 w-full" />
-            </div>
-          </CardContent>
-        </Card>
-        <div className="flex flex-col w-full gap-3">
+      <div className="flex flex-col w-full space-y-4">
           <Card className="space-y-2 p-4">
             <Skeleton className="h-4 w-[300px]" />
             <Skeleton className="h-4 w-[800px]" />
@@ -40,7 +29,6 @@ export default function ProblemsLoader() {
             <Skeleton className="h-4 w-[700px]" />
             <Skeleton className="h-4 w-[200px]" />
           </Card>
-        </div>
       </div>
     </div>
   );
